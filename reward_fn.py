e078fe06cc26e48bbbdfda3e012f9a5a02aa1820
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2023-02-03 13:55:34 +0200

    Multivariate

--- reward_fn.py
+++ reward_fn.py
@@ -1,6 +1,6 @@
 from data_loading import StateData
 import torch
-from torch.distributions import Normal
+from torch.distributions import MultivariateNormal
 import numpy as np
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -11,9 +11,6 @@ dataset = StateData("data/archive_10000.dat")
 i = torch.tensor(dataset.get_data()).to(device)
 decoder_mu, decoder_var, _, _ = vae.forward(i)
 for mu, log_var, state in zip(decoder_mu, decoder_var, i):
-    dist = Normal(mu, torch.exp(0.5 * log_var))
-    _, samples, _ = vae.sample(10, device)
-    for sample in samples:
-        print("Next state")
-        print(f"distance {np.linalg.norm(state.cpu() - sample.cpu().detach().numpy(), ord=2)}")
-        print(f"reward {np.sum(dist.log_prob(sample).cpu().detach().numpy())}")
+    dist = MultivariateNormal(mu, torch.diag(torch.exp(0.5 * log_var)))
+    for sample in i:
+        print(np.linalg.norm(state.cpu() - sample.cpu().detach().numpy(), ord=2), '->', dist.log_prob(sample).cpu().detach().numpy())
