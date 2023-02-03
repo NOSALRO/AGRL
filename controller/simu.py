638212df1da51b4ea8db6a95a4b9e2e61815c158
Constantinos Tsakonas <iamtsac@gmail.com>
2023-02-03 23:13:37 +0200

    model update needs fix

--- controller/simu.py
+++ controller/simu.py
@@ -12,19 +12,30 @@ sys.path.append("../")
 from data_loading import StateData
 from vae_arch import VarAutoencoder
 
-def opt(net, reward):
+def reward_fn(position, target, dist=None):
+    # reward = dist.log_prob(observations)
+    reward = np.linalg.norm(position.reshape(1,-1)[0] - target, ord=2)
+    return reward
+
+def get_model_params(model):
+    params = []
+    with torch.no_grad():
+        for i in model.parameters():
+            params.append(i.detach().numpy())
+    return params
+
+def set_model_params(model, params):
     pass
 
-def run_simu(robot, net, target, dt):
-    reward = 0
-    robot.step(np.zeros((3,1), dtype=np.float32), np.zeros((3,1), np.float32), dt)
-    for _ in range(30):
-        observations = torch.tensor(robot.state()).float().view(1, -1)
-        reward += dist.log_prob(observations)
-        opt(net, reward)
-        action = net(observations)
-        action = action.detach().numpy()
-        robot.step(action[0].reshape(3,1), action[1].reshape(3,1), dt)
+def run_simu_once(model_params, robot, net, target, dist, dt, reward):
+    # set_model_params(net, model_params)
+    observations = torch.tensor(robot.state()).float().view(1, -1)
+    action = net(observations)
+    action = action.detach().numpy()
+    robot.step(action[0].reshape(3,1), action[1].reshape(3,1), dt)
+    reward += reward_fn(robot.get_position(), target)
+    print(reward)
+    return reward
 
 
 MASS = 8
@@ -41,5 +52,9 @@ random_state = dataset.get_data()[0]
 target = vae.encoder(torch.tensor(random_state))[0]
 mu, log_var = vae.decoder(target)
 dist = MultivariateNormal(mu, torch.diag(torch.exp(0.5 * log_var)))
+target = np.random.random(3) + 10
 
-run_simu(rob, net, target, 0.1)
+reward = 0
+model_params = get_model_params(net)
+
+scipy.optimize.minimize(run_simu_once, x0=model_params, args=(model_params, rob, net, target, dist, 0.1, reward))
