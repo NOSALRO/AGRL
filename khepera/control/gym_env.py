7171faff19e8680a4bb868e6c1a933ea9110cf84
Constantinos Tsakonas <iamtsac@gmail.com>
2023-02-20 15:51:52 +0200

    Minor changes, added cli for faster testing

--- khepera/control/gym_env.py
+++ khepera/control/gym_env.py
@@ -18,17 +18,18 @@ class KheperaEnv(gym.Env):
     ):
         super().__init__()
         self.dist = reward_dist
-        self.enable_graphics = enable_graphics
+        self.enable_graphics = False
         self.radius = radius
         self.posture = fastsim.Posture(*posture)
         self.map = fastsim.Map(map_path, 600)
         self.robot = fastsim.Robot(radius, self.posture)
         self.max_steps = max_steps
         self.min, self.max = min_max_scale
-        self.action_space = gym.spaces.Box(low=-1, high=1, shape=(2,), dtype=np.float32)
+        self.action_space = gym.spaces.Box(low=-1., high=1., shape=(2,), dtype=np.float32)
         self.observation_space = gym.spaces.Box(low=np.array([0, 0, -np.pi]), high=np.array([600, 600, np.pi]), shape=(3,), dtype=np.float32)
+        self.ep_reward = 0
 
-    def step(self, action):
+    def step(self, action, *, eval=False):
         self.it += 1
         if self.enable_graphics:
             self.disp.update()
@@ -38,27 +39,33 @@ class KheperaEnv(gym.Env):
         # reward = self.dist.log_prob(scaled_obs).cpu().item()
         reward = np.linalg.norm(observation[:2] - self.target[:2], ord=2)
         done = False
-        if np.linalg.norm(observation[:2] - self.target[:2], ord=2) == 0 or self.max_steps == self.it:
-            done = True
+        if eval:
+            if np.linalg.norm(observation[:2] - self.target[:2], ord=2) == 0:
+                done = True
+        else:
+            if np.linalg.norm(observation[:2] - self.target[:2], ord=2) == 0 or self.max_steps == self.it:
+                done = True
 
-        return observation, -reward, done, {}
+        return np.array(observation, dtype=np.float32), -reward, done, {}
 
     def set_target(self, target_pos):
         self.target = target_pos
         if self.enable_graphics:
-            self.map.add_goal(fastsim.Goal(*self.target[:2], 10, 1))
+            self.map.add_goal(fastsim.Goal(*self.target[:2], 20, 1))
             self.disp.update()
 
     def reset(self):
         self.it = 0
         self.robot.set_pos(self.posture)
+        self.robot.move(0, 0, self.map, False)
         if self.enable_graphics:
             self.disp.update()
-        return self.__observation()
+        return np.array(self.__observation(), dtype=np.float32)
 
     def render(self):
         self.disp = fastsim.Display(self.map, self.robot)
         self.disp.update()
+        self.enable_graphics = True
 
     def __observation(self):
         _rpos = self.robot.get_pos()
--- khepera/control/rl.py
+++ khepera/control/rl.py
@@ -1,28 +1,119 @@
 import sys
+import argparse
 import numpy as np
 import torch
 from gym_env import KheperaEnv
 from utils import get_model_params
+import stable_baselines3
 from stable_baselines3 import SAC, PPO
+from stable_baselines3.common.env_checker import check_env
 
 sys.path.append('../vae/')
 from vae_arch import VAE, VariationalDecoder, VariationalEncoder
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-datapoints = np.loadtxt("../data_collection/datasets/three_wall.dat")
-vae = torch.load("../models/vae_three_wall.pt")
 
+# Parse arguments.
+parser = argparse.ArgumentParser()
+parser.add_argument("-a", "--algorithm", help="choose RL algorithm (PPO/SAC).")
+parser.add_argument("-m", "--mode", default='train', help="choose between train and eval mode.")
+parser.add_argument("-g", "--graphics", action='store_true', help="enable graphics during training.")
+parser.add_argument("--map", default="three_wall", help="choose map. Default: three_wall")
+args = parser.parse_args()
+algorithm = args.algorithm
+mode = args.mode
+graphics = args.graphics
+map_name = args.map
+
+
+# Load dataset and VAE.
+datapoints = np.loadtxt(f"../data_collection/datasets/{map_name}.dat")
+vae = torch.load(f"../models/vae_{map_name}.pt")
 target = datapoints[10000]
 x_hat, x_hat_var, latent, _ = vae(torch.tensor(target, device=device).float(), device, True, True)
 distribution = torch.distributions.MultivariateNormal(x_hat.cpu(), torch.diag(x_hat_var.mul(.5).exp().cpu()))
 
-env = KheperaEnv(True, reward_dist=distribution, min_max_scale=[vae.min, vae.max], max_steps=1000, posture=[100,50,0])
-env.render()
-env.set_target(target)
-policy_kwargs = dict(activation_fn=torch.nn.Tanh,
-                    net_arch=dict(pi=[16, 16], qf=[16, 16]))
+# Create Environment.
+env = KheperaEnv(
+    reward_dist=distribution,
+    min_max_scale=[vae.min, vae.max],
+    max_steps=800,
+    posture=target-100,
+    map_path=f"../data_collection/worlds/{map_name}.pbm"
+)
 
-# model = SAC('MlpPolicy', env, 5e-4, verbose=1, policy_kwargs=policy_kwargs)
-# model = SAC('MlpPolicy', env, 1e-4, verbose=1, batch_size=256)
-model = SAC('MlpPolicy', env, 1e-4, verbose=1, batch_size=256, policy_kwargs=dict(activation_fn=torch.nn.Tanh))
-model.learn(total_timesteps=100000)
+# Set up graphics.
+if graphics:
+    env.render()
+    env.set_target(target)
+
+# Set up RL algorithm.
+if algorithm.lower() == 'sac':
+    policy_kwargs = dict(net_arch=dict(pi=[16, 16], qf=[32, 16]))
+    model = SAC(
+        'MlpPolicy',
+        env,
+        learning_rate=0.0003,
+        verbose=1,
+        batch_size=128,
+        policy_kwargs=policy_kwargs,
+        learning_starts=150,
+        tau=0.005,
+        train_freq=1,
+        gradient_steps=1,
+        target_update_interval=1
+    )
+elif algorithm.lower() == 'ppo':
+    model = PPO(
+        'MlpPolicy',
+        env,
+        learning_rate=1e-4,
+        n_steps=1024,
+        batch_size=256,
+        n_epochs=10,
+        gamma=0.99,
+        gae_lambda=0.95,
+        clip_range=0.2,
+        clip_range_vf=None,
+        normalize_advantage=True,
+        ent_coef=0.0,
+        vf_coef=0.5,
+        max_grad_norm=0.5,
+        use_sde=False,
+        sde_sample_freq=2,
+        target_kl=0.5,
+        policy_kwargs=None,
+        verbose=1,
+        seed=None,
+        device='auto'
+    )
+
+# Set up mode.
+if mode.lower() == 'train':
+    try:
+        model.learn(total_timesteps=1e+4)
+    except KeyboardInterrupt:
+        print("Training stopped!")
+
+    model.save(algorithm)
+    env.render()
+    env.set_target(target)
+    obs = env.reset()
+    while True:
+        action, _states = model.predict(obs, deterministic=True)
+        obs, rewards, dones, info = env.step(action)
+
+elif mode.lower() == 'eval':
+    if algorithm.lower() == 'sac':
+        model = SAC.load('sac')
+    elif algorithm.lower() == 'ppo':
+        model = PPO.load('ppo')
+
+    env.render()
+    env.set_target(target)
+    observation = env.reset()
+    while True:
+        action, _ = model.predict(observation, deterministic=True)
+        observation, _, done, _ = env.step(action, eval=True)
+        if done:
+            print("Enviroment Solved!")
