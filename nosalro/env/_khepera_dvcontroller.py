b1442186f5959a33b792a36c179312c339a4d256
Constantinos Tsakonas <tsakonas.constantinos@gmail.com>
2023-04-11 23:48:52 +0300

    Fix numpy array multiplication

--- nosalro/env/_khepera_dvcontroller.py
+++ nosalro/env/_khepera_dvcontroller.py
@@ -45,7 +45,6 @@ class KheperaDVControllerEnv(KheperaEnv):
         self._controller(action)
 
     def _controller(self, action):
-        # tmp_target = self.observation_space.unscale(np.array(tmp_target), -1, 1)
         for _ in range(5):
             cmds = self.low_level_controller.update(action)
             self.robot.move(*cmds, self.world_map, False)
@@ -58,25 +57,25 @@ class KheperaDVControllerEnv(KheperaEnv):
 
     def _reward_fn(self, *args):
         observation, action = args
-        collision = int(self.robot.get_collision())
-        self.collision_weight += .0005 * collision
-        self.collision_weight *= collision
+        act = np.linalg.norm(action)
         if self.reward_type == 'distance':
-            act = np.linalg.norm(action)
-            dist = np.linalg.norm(observation[:2]*600 - self.target[:2])
-            return np.exp(-dist/self.sigma_sq) - (self.action_weight * act) - (self.collision_weight * np.log(1+dist))
+            dist = np.linalg.norm(np.multiply(observation[:2], 600) - self.target[:2])
+            return np.exp(-dist/self.sigma_sq) - (self.action_weight * act)
         elif self.reward_type == 'edl':
-            if len(self.scaler.mean) == 4:
-                scaled_obs = torch.tensor(self.scaler(observation[:self.n_obs]*600)) if self.scaler is not None else observation
+            if len(self.scaler.mean) == 2:
+                scaled_obs = torch.tensor(self.scaler([observation[0]*600, observation[1]*600])) if self.scaler is not None else observation
+            elif len(self.scaler.mean) == 4:
+                scaled_obs = torch.tensor(self.scaler([observation[0]*600, observation[1]*600, observation[2], observation[3]])) if self.scaler is not None else observation
             elif len(self.scaler.mean) == 3:
                 scaled_obs = torch.tensor(self.scaler(self._state())) if self.scaler is not None else observation
-            act = np.linalg.norm(action)
-            reward = np.exp(self.dist.log_prob(scaled_obs[:self.n_obs]).cpu().item()/self.sigma_sq) - self.action_weight * act
+            reward = np.exp(self.dist.log_prob(scaled_obs[:self.n_obs]).cpu().item()/self.sigma_sq) - (self.action_weight * act)
             return reward
 
     def _observations(self):
         _rpos = self._state()
-        if self.n_obs == 4:
+        if self.n_obs == 2:
+            _obs = [_rpos[0]/600, _rpos[1]/600]
+        elif self.n_obs == 4:
             _obs = [_rpos[0]/600, _rpos[1]/600, np.cos(_rpos[2]), np.sin(_rpos[2])]
         elif self.n_obs == 3:
             _obs = [_rpos[0]/600, _rpos[1]/600, _rpos[2]]
