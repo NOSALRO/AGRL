7e769b6328d499adc048359b429566c41d74c98b
Constantinos Tsakonas <tsakonas.constantinos@gmail.com>
2023-04-14 20:15:13 +0300

    Tests updated

---
+++ tests/test_eval_data.py
@@ -0,0 +1,13 @@
+import sys
+import torch
+from nosalro.vae import StatesDataset, VariationalAutoencoder, train, visualize
+from nosalro.transforms import Compose, AngleToSinCos, Scaler, Shuffle
+import numpy as np
+import pyfastsim as fastsim
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+dataset = StatesDataset(np.loadtxt(sys.argv[1]))
+vae = torch.load(sys.argv[2])
+i, x_hat_var, mu, logvar = vae(torch.tensor(dataset[900]).to(device), device, True, scale=True)
+print(mu)
+# visualize([dataset[:], i.detach().cpu().numpy()], projection='2d', file_name='.tmp/image')
--- tests/test_rl.py
+++ tests/test_rl.py
@@ -1,87 +1,83 @@
-import numpy as np
 import torch
-import gym
-from nosalro.env import KheperaEnv, Box
+import numpy as np
+from nosalro.env import KheperaDVControllerEnv, Box
+from nosalro.controllers import DVController
+from nosalro.rl.td3 import Actor, Critic, train_td3
 from nosalro.vae import StatesDataset, VariationalAutoencoder, train, visualize
 from nosalro.transforms import Compose, AngleToSinCos, Scaler, Shuffle
-from nosalro.rl.utils import train_sb3, cli
+from nosalro.rl.utils import  cli
+from nosalro.rl.sac import train_sac
+from nosalro.rl.ppo import train_ppo
+from nosalro.rl.td3 import train_td3
 import pyfastsim as fastsim
 
 
-if __name__ == '__main__':
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    scaler = Scaler()
-    target_scaler = Scaler(_type='standard')
-    shuffle = Shuffle(seed=42)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    transforms = Compose([
-        shuffle,
-        AngleToSinCos(angle_column=2),
-        scaler.fit,
-        scaler
-    ])
-    dataset = StatesDataset(path='data/no_wall.dat', transforms=transforms)
+scaler = Scaler()
+shuffle = Shuffle(seed=42)
+transforms = Compose([shuffle, scaler.fit, scaler])
 
-    # In case output dims is different from input dims.
-    target_transforms = Compose([
-        shuffle,
-        target_scaler.fit,
-        target_scaler
-    ])
-    target_dataset = StatesDataset(path='data/no_wall.dat', transforms=target_transforms)
+dataset = StatesDataset(np.loadtxt('data/go_explore_xy.dat')[:,:2], transforms=transforms)
 
-    vae = VariationalAutoencoder(input_dims=4, latent_dims=4, output_dims=4, hidden_sizes=[32,32], scaler=scaler).to(device)
-    vae = train(
-        model = vae,
-        epochs = 1000,
-        lr = 3e-04,
-        dataset = dataset,
-        device = device,
-        beta = 10,
-        file_name = 'models/vae_models/no_wall_vae_cos_sin.pt',
-        overwrite = False,
-        weight_decay = 0,
-        batch_size = 1024,
-        # target_dataset = target_dataset
-    )
-    # i, x_hat_var, mu, logvar = vae(torch.tensor(dataset[:]).to(device), device, True, scale=False)
-    # visualize([dataset[:], i.detach().cpu().numpy()], projection='2d')
+vae = VariationalAutoencoder(input_dims=2, latent_dims=2, output_dims=2, hidden_sizes=[64,64], scaler=scaler).to(device)
+vae = train(
+    model = vae,
+    epochs = 2000,
+    lr = 1e-04,
+    dataset = dataset,
+    device = device,
+    beta = 10,
+    file_name = 'models/vae_models/dots_vae_xy.pt',
+    overwrite = False,
+    weight_decay = 0,
+    batch_size = 1024,
+    # target_dataset = target_dataset
+)
+# i, x_hat_var, mu, logvar = vae(torch.tensor(dataset[:]).to(device), device, True, scale=False)
+# visualize([dataset[:], i.detach().cpu().numpy()], projection='2d', file_name='.tmp/image')
 
-    # Env Init.
-    world_map = fastsim.Map('worlds/no_wall.pbm', 600)
-    robot = fastsim.Robot(10, fastsim.Posture(100., 100., 0.))
-    dataset.inverse([scaler])
-    # target_dataset.inverse([target_scaler])
+# Env Init.
+world_map = fastsim.Map('worlds/dots.pbm', 600)
+robot = fastsim.Robot(10, fastsim.Posture(500., 500., 0.))
+dataset.inverse([scaler])
 
-    action_space = Box(low=-1., high=1., shape=(2,), dtype=np.float32)
-    observation_space = Box(
-        low=np.array([0, 0, -1, -1, -np.inf, -np.inf, -np.inf, -np.inf]),
-        high=np.array([600, 600, 1, 1, np.inf, np.inf, np.inf, np.inf]),
-        shape=(8,),
-        dtype=np.float32
-    )
-    start_space = Box(
-        low=np.array([25, 25, -np.pi]),
-        high=np.array([575, 575, np.pi]),
-        shape=(3,),
-        dtype=np.float32
-    )
+action_space = Box(low=-1., high=1., shape=(2,), dtype=np.float32)
+observation_space = Box(
+    low=np.array([0, 0, -np.pi, -np.pi, -np.inf, -np.inf]),
+    high=np.array([1, 1, np.pi, np.pi, np.inf, np.inf]),
+    shape=(6,),
+    dtype=np.float32
+)
+start_space = Box(
+    low=np.array([25, 25, -np.pi]),
+    high=np.array([575, 575, np.pi]),
+    shape=(3,),
+    dtype=np.float32
+)
 
-    env = KheperaEnv(
-        robot=robot,
-        world_map=world_map,
-        reward_type='edl',
-        n_obs=4,
-        goals=dataset[[100]],
-        goal_conditioned_policy=True,
-        latent_rep=True,
-        observation_space=observation_space,
-        action_space=action_space,
-        random_start=start_space,
-        max_steps=0,
-        vae=vae,
-        scaler=scaler
-    )
+env = KheperaDVControllerEnv(
+    robot=robot,
+    world_map=world_map,
+    reward_type='distance',
+    n_obs=4,
+    goals=dataset[:],
+    goal_conditioned_policy=True,
+    latent_rep=True,
+    observation_space=observation_space,
+    action_space=action_space,
+    random_start=False,
+    max_steps=100,
+    vae=vae,
+    scaler=scaler,
+    controller=DVController(),
+    sigma_sq=100
+)
 
-    # # Agent Train.
-    train_sb3(env, device, **cli())
+actor_net = Actor(observation_space.shape[0], action_space.shape[0], 1)
+critic_net = Critic(observation_space.shape[0], action_space.shape[0])
+
+# train_sac(env, actor_net, critic_net, eval_data=np.loadtxt('.tmp/test_data'))
+train_td3(env, actor_net, critic_net, eval_data=np.loadtxt('data/eval_data/dots.dat'))
+
+# eval_policy('models/policies/td3_test')
