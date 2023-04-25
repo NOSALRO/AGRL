7e00bb11d694b2527a32ea691644192c3ba7d845
Constantinos Tsakonas <tsakonas.constantinos@gmail.com>
2023-04-25 16:11:58 +0300

    Scheduler test

---
+++ tests/test_scheduler.py
@@ -0,0 +1,12 @@
+from nosalro.rl.utils import scheduler
+
+class Test:
+    def __init__(self):
+        self.sigma = 100
+        self.q = 50
+
+t = Test()
+for _ in range(2):
+    scheduler(t, ['sigma', 'q'], [0.5, 2])
+print(t.sigma)
+print(t.q)
