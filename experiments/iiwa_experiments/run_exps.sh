daa53eb5cf51cbc5850c920a1c1be8398266dea1
Constantinos Tsakonas <tsakonas.constantinos@gmail.com>
2023-05-01 18:11:57 +0300

    Experiments update

--- experiments/iiwa_experiments/run_exps.sh
+++ experiments/iiwa_experiments/run_exps.sh
@@ -2,8 +2,8 @@
 for exp_no in `seq 1 10`
 do
     echo "Running iiwa_experiments/iiwa_ge.py: ${exp_no}"
-    python experiments/iiwa_experiments/iiwa_ge.py       --file-name models/policies/iiwa/iiwa_ge_${exp_no}     --steps 200 --episodes 15000 --actor-lr 1e-3 --critic-lr 1e-3 --start-episode 30 --checkpoint-episodes 100 --expl-noise 0.2 --batch-size 256 --scheduling-episode 0
+    # python experiments/iiwa_experiments/iiwa_ge.py       --file-name models/policies/iiwa_new/iiwa_ge_${exp_no}     --steps 200 --episodes 2000 --actor-lr 1e-3 --critic-lr 1e-3 --start-episode 10 --checkpoint-episodes 40 --expl-noise 0.2 --batch-size 256 --scheduling-episode 0
 
     echo "Running iiwa_experiments/iiwa_uniform.py: ${exp_no}"
-    python experiments/iiwa_experiments/iiwa_uniform.py  --file-name models/policies/iiwa/iiwa_unfirom_${exp_no} --steps 200 --episodes 15000 --actor-lr 1e-3 --critic-lr 1e-3 --start-episode 30 --checkpoint-episodes 100 --expl-noise 0.2 --batch-size 256 --scheduling-episode 0
+    python experiments/iiwa_experiments/iiwa_uniform.py  --file-name models/policies/iiwa_new/iiwa_uniform_${exp_no} --steps 200 --episodes 4000 --actor-lr 1e-3 --critic-lr 1e-3 --start-episode 10 --checkpoint-episodes 20 --expl-noise 0.2 --batch-size 256 --scheduling-episode 0
 done
